df24b2053315d3f241c03cbd463d92ea226ebc70
ChaitraYallari <141244512+ChaitraYallari@users.noreply.github.com>
2023-11-06 20:12:04 +0530

    Update C-Scan Disk Scheduling Algorithm.c

--- C-Scan Disk Scheduling Algorithm.c
+++ C-Scan Disk Scheduling Algorithm.c
@@ -1,52 +1,62 @@
-#include<stdio.h>
-main()
-{
-int t[20], d[20], h, i, j, n, temp, k, atr[20], tot, p, sum=0;
-printf("enter the no of tracks to be traveresed");
-scanf("%d'",&n);
-printf("enter the position of head");
-scanf("%d",&h);
-t[0]=0;t[1]=h;
-printf("enter total tracks");
-scanf("%d",&tot);
-t[2]=tot-1;
-printf("enter the tracks");
-for(i=3;i<=n+2;i++)
-scanf("%d",&t[i]);
-for(i=0;i<=n+2;i++)
-for(j=0;j<=(n+2)-i-1;j++)
-if(t[j]>t[j+1])
-{
-for(i=0;i<=n+2;i++)
-if(t[i]==h);
-j=i;break;
-temp=t[j];
-t[j]=t[j+1];
-t[j+1]=temp;
-}
-p=0;
-while(t[j]!=tot-1)
-{
-atr[p]=t[j];
-j++;
-p++;
-}
-atr[p]=t[j];
-p++;
-i=0;
-while(p!=(n+3) && t[i]!=t[h])
-{
-atr[p]=t[i]; i++;
-p++;
-}
-for(j=0;j<n+2;j++)
-{
-if(atr[j]>atr[j+1])
-d[j]=atr[j]-atr[j+1];
-else
-d[j]=atr[j+1]-atr[j];
-sum+=d[j];
-}
-printf("total header movements%d",sum);
-printf("avg is %f",(float)sum/n);
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+ int RQ[100], i, j, n, TotalHeadMoment = 0, initial, size, move;
+ printf("Enter the number of Requests\n");
+ scanf("%d", &n);
+ printf("Enter the Requests sequence\n");
+ for (i = 0; i < n; i++)
+ scanf("%d", &RQ[i]);
+ printf("Enter initial head position\n");
+ scanf("%d", &initial);
+ printf("Enter total disk size\n");
+ scanf("%d", &size);
+ printf("Enter the head movement direction for high 1 and for low 
+0\n");
+ scanf("%d", &move);
+ for (i = 0; i < n; i++){
+ for (j = 0; j < n - i - 1; j++){
+ if (RQ[j] > RQ[j + 1]){
+ int temp;
+ temp = RQ[j];
+ RQ[j] = RQ[j + 1];
+ RQ[j + 1] = temp;
+ }
+ }
+ }
+ int index;
+ for (i = 0; i < n; i++){
+ if (initial < RQ[i]){
+ index = i;
+ break;
+ }
+ }
+ if (move == 1){
+ for (i = index; i < n; i++){
+ TotalHeadMoment = TotalHeadMoment + abs(RQ[i] - initial);
+ initial = RQ[i];
+ }
+ TotalHeadMoment = TotalHeadMoment + abs(size - RQ[i - 1] - 1);
+ TotalHeadMoment = TotalHeadMoment + abs(size - 1 - 0);
+ initial = 0;
+ for (i = 0; i < index; i++){
+ TotalHeadMoment = TotalHeadMoment + abs(RQ[i] - initial);
+ initial = RQ[i];
+ }
+ }
+ else{
+ for (i = index - 1; i >= 0; i--){
+ TotalHeadMoment = TotalHeadMoment + abs(RQ[i] - initial);
+ initial = RQ[i];
+ }
+ TotalHeadMoment = TotalHeadMoment + abs(RQ[i + 1] - 0);
+ TotalHeadMoment = TotalHeadMoment + abs(size - 1 - 0);
+ initial = size - 1;
+ for (i = n - 1; i >= index; i--){
+ TotalHeadMoment = TotalHeadMoment + abs(RQ[i] - initial);
+ initial = RQ[i];
+ }
+ }
+ printf("Total head movement is %d", TotalHeadMoment);
+ return 0;
 }
